e0a0851341406704dea7fc7f82161a3defb353ad
Amarraykar07 <raykaramar7@gmail.com>
2025-04-07 19:05:58 +0000

    1_variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,13 @@
+const Id = 23166
+let Email = "amar@gmail.com"
+var Pass = "123"
+City = "Ahilyanagar"
+
+// Id = 2 not allowed
+
+Email = "a@gmail.com"
+Pass = "890"
+City = "Pune"
+
+console.log([Id,Email,Pass,])
+console.table([Id,Email,Pass,])

e3f904a5d4d21a90548fee11132b34366ebe86b7
Amarraykar07 <raykaramar7@gmail.com>
2025-04-19 10:29:14 +0000

    Coversion is done

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,17 @@
+/*console.log("A")
+let s = 66
+console.log(typeof s)
+
+let num = Number(s)
+console.log(num) */
+
+//"66" => 66
+// "66abs" => NaN
+// true => 1 ; false => 0
+
+let B = "Amar"
+let b = Boolean(B)
+console.log(b)
+
+// " " => false
+//"Amar" => True
